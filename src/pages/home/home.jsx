4bee8a325c4d6c48b787d1e02940c8aa75b872f8
diegohuertan <124300301+diegohuertan@users.noreply.github.com>
2023-11-24 16:01:28 -0300

    cambios home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,32 +2,18 @@ import React from "react";
 import PageContainer from "../../components/container/PageContainer";
 import { Box } from "@mui/material";
 import Carousel from "./componentes/carrusel";
-import { Grid, Typography } from "@mui/material";
 
 
 function Home() {
 
   return (
-    <PageContainer title="Pagina inicio" description="" >
-      <Box height="100%">
+<PageContainer>
+<Box height="100%">
         <Carousel />
       </Box>
-    <PageContainer title="Pagina inicio" description="aaaaaaaaaaaaaaaaa" >
-      <div
-        style={{
-          backgroundColor: "#757ce8",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-          width: "100%",
-        }}
-      >
-        <h1>Home</h1>
-        
-      </div>
-
     </PageContainer>
+    
+
   );
 }
 
